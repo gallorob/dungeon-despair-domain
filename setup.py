830fc8cbeb77c97e56bbc7f6f0b4dd0c545cecf0
gallorob <32876132+gallorob@users.noreply.github.com>
2025-05-27 13:24:13 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='dungeon_despair_domain',
       description='Dungeon Despair Domain',
       long_description='',
       version='0.0.1',
-      python_requires='>=3.11',
+      python_requires='>=3.10',
       install_requires=['pydantic', 'pyyaml', 'gptfunctionutil'],
       packages=['dungeon_despair'],
       )
